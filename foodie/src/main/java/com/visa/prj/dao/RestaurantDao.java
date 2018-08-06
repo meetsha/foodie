97cd9ed82500fefe40b3d577d783f326052bc214
rachana <rachansk1997@gmail.com>
2018-08-06 11:33:05 +0100

    restaurant dao

---
+++ foodie/src/main/java/com/visa/prj/dao/RestaurantDao.java
@@ -0,0 +1,13 @@
+package com.visa.prj.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.visa.prj.entity.Restaurant;
+
+public interface RestaurantDao extends JpaRepository<Restaurant, Integer> {
+	@Query("from Restaurant r where r.pureveg = 1")
+	List<Restaurant> getVegRestaurants();
+}
