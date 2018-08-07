108fb3dcf755b5cfd37b60f9ceb15633315fd83a
shivani <shivanireddynalla17@gmail.com>
2018-08-07 06:34:10 +0100

    menu  functionalities added

--- foodie/src/main/java/com/visa/prj/service/FoodieService.java
+++ foodie/src/main/java/com/visa/prj/service/FoodieService.java
@@ -1,11 +1,16 @@
 package com.visa.prj.service;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.visa.prj.dao.MenuDao;
 import com.visa.prj.dao.UserDao;
+import com.visa.prj.entity.Menu;
 import com.visa.prj.entity.User;
 
 @Service
@@ -17,5 +22,31 @@ public class FoodieService {
 	@Transactional
 	public void addUser(User user) {
 		userDao.save(user);
+	}
+	@Autowired
+	private MenuDao menuDao;
+	
+	@Transactional
+	public void addMenuItem(Menu menu) {
+		menuDao.save(menu);
+	}
+	
+	public Menu getById(int id) {
+		return menuDao.findById(id).get();
+	}
+	
+	public List<Menu> getAvailableMenuItems() {
+		return menuDao.findAll();
+	}
+	
+	public List<Menu> getAvailableVegItems(boolean veg){
+		return menuDao.getAvailableVegItems(veg);
+	}
+
+	
+	@Transactional
+	public void deleteMenuItem(int id) {
+		Menu m = menuDao.findById(id).get();
+		menuDao.delete(m);
 	}
 }
