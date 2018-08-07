5e64f3a04bb0037c14c1925db6aba58c0fd2f1b6
Varun596 <s.e.varun@gmail.com>
2018-08-07 07:46:20 +0100

    Order functionalies added

--- foodie/src/main/java/com/visa/prj/service/FoodieService.java
+++ foodie/src/main/java/com/visa/prj/service/FoodieService.java
@@ -1,11 +1,15 @@
 package com.visa.prj.service;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.visa.prj.dao.OrdersDao;
 import com.visa.prj.dao.UserDao;
+import com.visa.prj.entity.Order;
 import com.visa.prj.entity.User;
 
 @Service
@@ -17,5 +21,17 @@ public class FoodieService {
 	@Transactional
 	public void addUser(User user) {
 		userDao.save(user);
+	}
+	
+	@Autowired
+	private OrdersDao orderDao;
+	
+	public List<Order> getOrders() {
+		return orderDao.findAll();
+	}
+	
+	@Transactional
+	public void addOrder(Order order) {
+		orderDao.save(order);
 	}
 }
